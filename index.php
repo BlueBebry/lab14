c158ad31255d5d876886b4c357532b8411c4c667
BlueBebry <_email@example.com>
2026-04-08 16:02:29 +0300

    добавлена главная страница

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php phpinfo(); ?>
+<?php
+class Page {
+    protected string $name = "home";
+    protected string $template = "<h1>Добро пожаловать!</h1><p>Выберите свою сторону силы ниже.</p>";
+
+    public function render(): void {
+        echo "<div class='container'>{$this->template}</div>";
+    }
+}
+
+echo "<nav>
+    <a href='?page=page'>Главная</a> | 
+    <a href='?page=cats'>Коты</a> | 
+    <a href='?page=dogs'>Собаки</a>
+</nav><hr>";
+
+$pageParam = $_GET['page'] ?? 'home';
+
+if ($pageParam === 'home' || $pageParam === 'page') {
+    $current = new Page();
+    $current->render();
+}
+?>
